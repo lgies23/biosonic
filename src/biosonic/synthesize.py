2ba54e63d7c7d55ca74e7a644087cd2d644730a8
lgies23 <a12113065@unet.univie.ac.at>
2025-05-13 17:00:57 +0200

    comment functions in progress

--- src/biosonic/synthesize.py
+++ src/biosonic/synthesize.py
@@ -1,17 +1,17 @@
-def ripple_sound():
-    """
-    Generate a ripple signal ad defined in [2]:
+# def ripple_sound():
+#     """
+#     Generate a ripple signal ad defined in [2]:
 
-    Amplitude envelope of each frequency band:
-    .. math::
-        S_i(t,f)=A_icos(2\pi\omega_{t,i}t+2\pi\omega_{f,i}f+\phi_i)
+#     Amplitude envelope of each frequency band:
+#     .. math::
+#         S_i(t,f)=A_icos(2\pi\omega_{t,i}t+2\pi\omega_{f,i}f+\phi_i)
     
-    Spectrogram: 
-    .. math::
-        S(t,f)=A_0+\sum_i S_i(t,f)
+#     Spectrogram: 
+#     .. math::
+#         S(t,f)=A_0+\sum_i S_i(t,f)
 
-    2. Singh NC, Theunissen FE. 2003 Modulation spectra of natural sounds and ethological theories of auditory processing. 
-    The Journal of the Acoustical Society of America 114, 3394–3411. (doi:10.1121/1.1624067) 
+#     2. Singh NC, Theunissen FE. 2003 Modulation spectra of natural sounds and ethological theories of auditory processing. 
+#     The Journal of the Acoustical Society of America 114, 3394–3411. (doi:10.1121/1.1624067) 
     
-    """
-    pass
+#     """
+#     pass
