cbbe513c614b65be23b389564cf27875dd4d2488
lenag23 <lena.gies@univie.ac.at>
2026-01-02 14:27:58 +0100

    readthedocs config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,21 +12,21 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 
 project = 'BioSonic'
 copyright = '2025, Lena Gies, Tecumseh Fitch'
-author = 'Lena Gies, Tecumseh Fitch'
-release = 'v0.0'
+author = 'Lena Gies, Tecumseh Fitch, Yannick Jadoul'
+release = 'v0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
+extensions: list[str] = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',         # Google/NumPy style docstrings
     'sphinx.ext.viewcode',         # source code
     'sphinx_autodoc_typehints',    # type hints
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+templates_path: list[str] = ['_templates']
+exclude_patterns: list[str] = []
 
 intersphinx_mapping = {
     'numpy': ('https://numpy.org/doc/stable/', None),
@@ -38,4 +38,4 @@ autodoc_typehints = 'description'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path: list[str] = ['_static']
